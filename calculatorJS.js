dbb618679e4454268439719ef32f90a0675d38a0
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-12 17:07:54 -0600

    Add eventlisteners to buttons so that when pressed, their id is displayed on #display container

--- calculatorJS.js
+++ calculatorJS.js
@@ -1,15 +1,29 @@
 function add(a, b){
-    return a+b;
+    return a + b;
 }
 
 function subtract(a, b){
-    return a-b;
+    return a - b;
 }
 
 function multiply(a,b){
-    return a*b;
+    return a * b;
 }
 
 function divide(a, b){
-    return a/b
+    return a / b
 }
+
+function operate(func, a, b){
+    return func(a,b);
+}
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", () =>{
+        let textContent = button.textContent;
+        let display = document.getElementById("display");
+        display.textContent = textContent;
+    });
+})
